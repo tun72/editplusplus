9ecbbd040e7a3b78e07833c0e9213a6095379372
Tun72 <tunt72553@gmail.com>
2024-11-29 22:11:37 +0630

    fix

--- middleware.ts
+++ middleware.ts
@@ -75,31 +75,15 @@
 //   ],
 // };
 
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isTenantRoute = createRouteMatcher(['/organization-selector(.*)', '/orgid/(.*)'])
-
-const isTenantAdminRoute = createRouteMatcher(['/orgId/(.*)/memberships', '/orgId/(.*)/domain'])
-
-export default clerkMiddleware(async (auth, req) => {
-  // Restrict admin routes to users with specific permissions
-  if (isTenantAdminRoute(req)) {
-    await auth.protect((has) => {
-      return (
-        has({ permission: 'org:sys_memberships:manage' }) ||
-        has({ permission: 'org:sys_domains_manage' })
-      )
-    })
-  }
-  // Restrict organization routes to signed in users
-  if (isTenantRoute(req)) await auth.protect()
-})
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
-}
+};
